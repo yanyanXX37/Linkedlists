4249b8af98ed05e821575a5dc7da4805c25e81fc
yanyanXX37 <adrianofficial133@gmail.com>
2026-01-09 20:10:46 +0800

    Create Links.java

---
+++ Links.java
@@ -0,0 +1,22 @@
+class Node{
+    int age;
+    String name;
+    String school;
+    int id;
+    Node prev, next;
+
+    Node(int age, String name, String school, int id){
+        this.age = age;
+        this.name = name;
+        this.school = school;
+        this.id = id;
+    }
+}
+
+
+
+public class Linker {
+}
+
+public void main() {
+}
